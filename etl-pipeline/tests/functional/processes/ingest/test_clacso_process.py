7bb4fa0e0a29e6050dfe147e240d33f7a96c5b3f
Kyle Villegas <86266231+kylevillegas93@users.noreply.github.com>
2025-04-11 14:16:49 -0400

    SFR-2614: Add file save and record save steps to record pipeline (#682)

--- etl-pipeline/tests/functional/processes/ingest/test_clacso_process.py
+++ etl-pipeline/tests/functional/processes/ingest/test_clacso_process.py
@@ -1,12 +1,14 @@
-from processes import CLACSOProcess
+from processes import CLACSOProcess, RecordPipelineProcess
 from .assert_ingested_records import assert_ingested_records
 from .assert_uploaded_manifests import assert_uploaded_manifests
 
 
 def test_clacso_process():
     clacso_process = CLACSOProcess('complete', None, None, None, 5, None)
-
     number_of_records_ingested = clacso_process.runProcess()
+
+    record_pipeline_process = RecordPipelineProcess()
+    record_pipeline_process.runProcess(max_attempts=1)
 
     records = assert_ingested_records(source_name='clacso', expected_number_of_records=number_of_records_ingested)
     assert_uploaded_manifests(records)
