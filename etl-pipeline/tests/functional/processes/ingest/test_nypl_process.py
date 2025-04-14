0512d718950bb38ecb14db8b0bc01f941dae5d1a
Dmitri Slory <dmitrislory@nypl.org>
2025-04-14 15:08:29 -0400

    SFR-2618: NYPL Record Ingestor (#683)

--- etl-pipeline/tests/functional/processes/ingest/test_nypl_process.py
+++ etl-pipeline/tests/functional/processes/ingest/test_nypl_process.py
@@ -1,13 +1,13 @@
-import pytest
-import os
-from processes import NYPLProcess
+from processes import NYPLProcess, RecordPipelineProcess
 from .assert_ingested_records import assert_ingested_records
+from .assert_uploaded_manifests import assert_uploaded_manifests
 
-
-@pytest.mark.skipif(os.getenv('IS_CI') == 'true', reason="Skipping in CI environment")
 def test_nypl_process():
     nypl_process = NYPLProcess('complete', None, None, None, 5, None)
+    number_of_records_ingested = nypl_process.runProcess()
 
-    nypl_process.runProcess()
+    record_pipeline_process = RecordPipelineProcess()
+    record_pipeline_process.runProcess(max_attempts=1)
 
-    assert_ingested_records(source_name='nypl')
+    records = assert_ingested_records(source_name='nypl', expected_number_of_records=number_of_records_ingested)
+    assert_uploaded_manifests(records)
