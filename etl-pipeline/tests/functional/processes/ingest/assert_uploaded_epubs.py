62e19a048b0dbf4ec2c4bcb6772b3a8952e59c96
Dmitri <you@example.com>
2025-04-16 12:33:55 -0400

    Changed variable names in functional test

--- etl-pipeline/tests/functional/processes/ingest/assert_uploaded_epubs.py
+++ etl-pipeline/tests/functional/processes/ingest/assert_uploaded_epubs.py
@@ -6,9 +6,9 @@ def assert_uploaded_epubs(records: list[Record]):
     s3_manager = S3Manager()
 
     for record in records:
-        manifest_part = next((part for part in record.parts if part.file_type == 'application/epub+zip'), None)
+        epub_part = next((part for part in record.parts if part.file_type == 'application/epub+zip'), None)
 
-        if manifest_part and 'epub' in manifest_part.url:
-            manifest_head_response = s3_manager.client.head_object(Key=manifest_part.file_key, Bucket=manifest_part.file_bucket)
+        if epub_part and 'epub' in epub_part.url:
+            epub_head_response = s3_manager.client.head_object(Key=epub_part.file_key, Bucket=epub_part.file_bucket)
 
-            assert manifest_head_response is not None
+            assert epub_head_response is not None
