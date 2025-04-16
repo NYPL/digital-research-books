efb52570643b3ad726c3937cac86e6b463fa1dc3
Dmitri <you@example.com>
2025-04-16 13:19:34 -0400

    Decoupled source_service from LOCProcess

--- etl-pipeline/processes/ingest/loc.py
+++ etl-pipeline/processes/ingest/loc.py
@@ -13,7 +13,17 @@ class LOCProcess():
     def __init__(self, *args):
         self.loc_service = LOCService()
         self.params = utils.parse_process_args(*args)
-        self.record_ingestor = RecordIngestor(source_service=self.loc_service, source=Source.LOC.value)
-
-    def runProcess(self):    
-        return self.record_ingestor.ingest(params=self.params)
+        self.record_ingestor = RecordIngestor(source=Source.LOC.value)
+    
+    def runProcess(self) -> int:
+        start_timestamp = utils.get_start_datetime(
+            process_type=self.params.process_type,
+            ingest_period=self.params.ingest_period,
+        ),
+        return self.record_ingestor.ingest(
+            self.loc_service.get_records(
+                start_timestamp=start_timestamp,
+                offset=self.params.offset,
+                limit=self.params.limit,
+            ),
+        )
