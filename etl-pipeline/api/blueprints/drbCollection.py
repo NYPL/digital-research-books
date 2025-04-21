2fe197f96f6d5868ab2ff05bfe3f15f248cd255b
kyle <kylejacksonvillegas@nypl.org>
2025-04-21 13:20:50 -0400

    NO-REF: Don't build publications when getting collections

--- etl-pipeline/api/blueprints/drbCollection.py
+++ etl-pipeline/api/blueprints/drbCollection.py
@@ -409,7 +409,7 @@ def get_collections():
             uuid = collection.uuid
             path = '/collection/{}'.format(uuid)
 
-            group = constructOPDSFeed(collection, db_client, perPage=5, path=path)
+            group = constructOPDSFeed(collection, db_client, perPage=5, path=path, build_publications=False)
 
             opds_feed.addGroup(group)
 
@@ -441,7 +441,7 @@ def constructSortMethod(sort):
 
 
 def constructOPDSFeed(
-    collection, dbClient, sort=None, page=1, perPage=10, path=None
+    collection, dbClient, sort=None, page=1, perPage=10, path=None, build_publications: bool=True
 ):
     uuid = collection.uuid
 
@@ -461,13 +461,16 @@ def constructOPDSFeed(
         'rel': 'self', 'href': path, 'type': 'application/opds+json'
     })
 
-    if collection.type == "static":
-        _addStaticPubsToFeed(opdsFeed, collection, path, page, perPage, sort)
-    elif collection.type == "automatic":
-        esClient = ElasticClient(current_app.config["REDIS_CLIENT"])
-        _addAutomaticPubsToFeed(opdsFeed, dbClient, esClient, collection.id, path, page, perPage)
+    if build_publications:
+        if collection.type == "static":
+            _addStaticPubsToFeed(opdsFeed, collection, path, page, perPage, sort)
+        elif collection.type == "automatic":
+            esClient = ElasticClient(current_app.config["REDIS_CLIENT"])
+            _addAutomaticPubsToFeed(opdsFeed, dbClient, esClient, collection.id, path, page, perPage)
+        else:
+            raise ValueError(f"Encountered collection with unhandleable type {collection.type}")
     else:
-        raise ValueError(f"Encountered collection with unhandleable type {collection.type}")
+        opdsFeed.metadata.addField('numberOfItems', len(collection.editions))
 
     return opdsFeed
 
