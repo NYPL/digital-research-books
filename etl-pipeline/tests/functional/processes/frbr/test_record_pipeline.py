99b2c999971336b6bddd08564aa228a3e69de598
Sarang Joshi <sarangjoshi@nypl.org>
2025-04-25 13:40:42 -0400

    Migrate record pipeline to SQS
    Now that we've validated that the ingestor can successfully write to SQS, migrate the pipeline to read off the new queue.

--- etl-pipeline/tests/functional/processes/frbr/test_record_pipeline.py
+++ etl-pipeline/tests/functional/processes/frbr/test_record_pipeline.py
@@ -1,32 +1,26 @@
 import json
-import os
 from time import sleep
 
-from managers import RabbitMQManager, RedisManager
+from managers import RedisManager
 from model import Record
 from processes.record_pipeline import RecordPipelineProcess
 from tests.functional.processes.frbr.test_cluster_process import assert_record_clustered
 from tests.functional.processes.frbr.test_embellish_process import assert_record_embellished
 
 
-def test_record_pipeline(db_manager, rabbitmq_manager: RabbitMQManager, unembellished_pipeline_record_uuid, mock_epub_to_webpub):
+def test_record_pipeline(db_manager, unembellished_pipeline_record_uuid, mock_epub_to_webpub):
     redis_manager = RedisManager()
 
     redis_manager.create_client()
     redis_manager.clear_cache()
 
-    record_queue = os.environ['RECORD_PIPELINE_QUEUE']
-    record_route = os.environ['RECORD_PIPELINE_ROUTING_KEY']
-
-    rabbitmq_manager.create_or_connect_queue(record_queue, record_route)
-
     record_pipeline = RecordPipelineProcess()
 
-    record = db_manager.session.query(Record).filter(Record.uuid == unembellished_pipeline_record_uuid).first()
+    record = db_manager.session.query(Record).filter(
+        Record.uuid == unembellished_pipeline_record_uuid,
+    ).first()
 
-    rabbitmq_manager.send_message_to_queue(
-        queue_name=record_queue,
-        routing_key=record_route,
+    record_pipeline.sqs_manager.send_message_to_queue(
         message={ "source_id": record.source_id, "source": record.source }
     )
 
