3c4b66017970ed7303d4ac197651feba3ede6db0
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2025-04-28 19:17:34 +0200

    update cluster tracking

--- etl-pipeline/services/monitor.py
+++ etl-pipeline/services/monitor.py
@@ -1,10 +1,30 @@
 import newrelic.agent
+from model import Record
 
 
 def record_event(event_name: str, data: dict):
     newrelic.agent.record_custom_event(event_name, data)
 
 
-def track_work_records_chosen(record_id: str, num_records: int):
-    event_name = f"FRBR:WorkRecordsChosen"
-    record_event(event_name, {"num_records": num_records, "record.id": record_id})
+def record_record_event(record: Record, event_name: str, data: dict):
+    record_event(
+        event_name,
+        {
+            **data,
+            "record.id": record.id,
+            "record.title": record.title,
+            "record.source": record.source,
+            "record.identifiers": record.identifiers,
+        },
+    )
+
+
+def track_work_records_chosen(record: Record, num_records: int):
+    event_name = f"Cluster:WorkRecordsChosen"
+    record_record_event(
+        record,
+        event_name,
+        {
+            "num_records": num_records,
+        },
+    )
