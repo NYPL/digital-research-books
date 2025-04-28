4585d0b0280c090b5e23859c4227761aa67dd963
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2025-04-28 19:26:00 +0200

    change import style

--- etl-pipeline/processes/record_embellisher.py
+++ etl-pipeline/processes/record_embellisher.py
@@ -7,7 +7,7 @@ from mappings.oclcCatalog import CatalogMapping
 from managers import DBManager, OCLCCatalogManager, RedisManager
 from model import Record, RecordState
 from .record_buffer import RecordBuffer
-from services.monitor import track_oclc_related_records_found
+import services.monitor as Monitor
 
 
 logger = create_log(__name__)
@@ -73,7 +73,7 @@ class RecordEmbellisher:
             self._add_works(matches)
 
         # Track the event
-        track_oclc_related_records_found(
+        Monitor.track_oclc_related_records_found(
             record=record,
             num_matches=num_matches,
             fell_back_to_title_author=fell_back_to_title_author
