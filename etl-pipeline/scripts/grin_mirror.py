96b2643936a1c61a156947c957671bf0ff360336
Priya Pai <priyapai@nypl.org>
2025-05-06 13:04:44 -0400

    Add acquired_today method

--- etl-pipeline/scripts/grin_mirror.py
+++ etl-pipeline/scripts/grin_mirror.py
@@ -3,6 +3,8 @@
 # Additional notes - https://docs.google.com/document/d/1aZ5ODEzKP6qX1f4CCcGtlidRvr-Fu8HPA-AEhTwDTyk/edit?usp=sharing
 
 import os.path
+import pandas as pd
+from datetime import datetime, timedelta
 from google.auth.transport.requests import (
     AuthorizedSession,
 )
@@ -45,32 +47,42 @@ class GRINClient(object):
     def _url(self, fragment):
         return "https://books.google.com/libraries/NYPL/" + fragment
 
-    def get(self, url, filename, force=False):
-        cache_path = os.path.join(self.cache_dir, filename)
-        if os.path.exists(cache_path) and not force:
-            return open(cache_path, 'rb').read()
+    def get(self, url,force=False):
         url = self._url(url)
         response = self.session.request("GET", url)
         if response.status_code != 200:
             raise IOError("%s got %s unexpectedly" % (url, response.status_code))
-        open(cache_path, "wb").write(response.content)
         return response.content
+    
+    def acquired_today(self, *args, **kwargs):
+        # For GRIN queries, range start is inclusive but the range end is exclusive.
+        # This means you must set the upper range to one day after the desired date
+        today = datetime.now()
+        tomorrow = today + timedelta(1)
+        range_start = today.strftime("%Y-%m-%d")
+        range_end = tomorrow.strftime("%Y-%m-%d")
+        data = self.get("_monthly_report?execute_query=true&year=2025&month=5&check_in_date_start=%s&check_in_date_end=%s&format=text" %
+                        (range_start, range_end), "new")
+        data = data.decode("utf8").split("\n")
+        headers = data[0].split('\t')
+        df = pd.DataFrame(data[1].split('\t'), columns=headers)
+        return data
 
     def _for_state(self, state, *args, **kwargs):
-        "Which books are in the given state?"
-        data = self.get("_%s?format=text" % state, "%s.txt" %state, *args, **kwargs)
+        # Which books are in the given state?
+        data = self.get("_%s?format=text" % state, *args, **kwargs)
         return data.decode("utf8").strip().split("\n")
         
     def available(self, *args, **kwargs):
-        "Which barcodes are available for conversion?"
+        # Which barcodes are available for conversion?
         return self._for_state("available", *args, **kwargs)
 
     def in_process(self, *args, **kwargs):
-        "Which barcodes are being converted?"
+        # Which barcodes are being converted?
         return self._for_state("in_process", *args, **kwargs)
 
     def converted(self, *args, **kwargs):
-        "What are the filenames of files that have been converted?"
+        # What are the filenames of files that have been converted?
         return self._for_state("converted", *args, **kwargs)
 
     def failed(self, *args, **kwargs):
@@ -93,6 +105,7 @@ class GRINClient(object):
         self.session.request("POST", self._url("_process"), data=barcodes)
 
 client = GRINClient("cache")
+print(client.acquired_today())
 print("Number available: %s" % len(client.available()))
 print("Number failed: %s" % len(client.failed()))
 converted = client.converted()
