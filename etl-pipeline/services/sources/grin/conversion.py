eaa4622d8e49158818982cbe628e4191d972447f
toxiapo <toxiapo@gmail.com>
2025-05-06 12:58:46 -0400

    Conversion template

---
+++ etl-pipeline/services/sources/grin/conversion.py
@@ -0,0 +1,19 @@
+
+
+class GRINConversion:
+    def __init__(self):
+        # DB
+        # acquire 
+        pass
+
+    def acquire_and_convert(self):
+        # acquire the new books within the month
+        pass
+
+    def convert_backfills(self):
+        #  initialize conversion the new books, and update the DB
+        pass
+
+    def get_converted(self):
+        # scrape the converted files from GRIN and update the DB
+        pass
