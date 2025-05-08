e4cd34bcdc201f989f311e9982a6468c5b7822f5
toxiapo <toxiapo@gmail.com>
2025-05-08 12:22:50 -0400

    Add converted step

--- etl-pipeline/services/sources/grin/conversion.py
+++ etl-pipeline/services/sources/grin/conversion.py
@@ -1,10 +1,33 @@
+import logging
+import os
+from google.auth.transport.requests import (
+    AuthorizedSession,
+)
+from model import GRINState, GRINStatus, Record, FRBRStatus
+from managers import DBManager
+from google.oauth2.service_account import Credentials
+import json
+from services.ssm_service import SSMService
+from logger import create_log
+
+# TODO: Update this once the GRINClient class is ready
+from scripts.grin_mirror import GRINClient
+
+logger = create_log(__name__)
 
 
 class GRINConversion:
     def __init__(self):
-        # DB
-        # acquire 
-        pass
+        self.db_manager = DBManager(
+            user=os.environ.get("POSTGRES_USER", None),
+            pswd=os.environ.get("POSTGRES_PSWD", None),
+            host=os.environ.get("POSTGRES_HOST", None),
+            port=os.environ.get("POSTGRES_PORT", None),
+            db=os.environ.get("POSTGRES_NAME", None),
+        )
+        self.db_manager.create_session()
+
+        self.client = GRINClient()
 
     def acquire_and_convert(self):
         # acquire the new books within the month
@@ -14,6 +37,29 @@ class GRINConversion:
         #  initialize conversion the new books, and update the DB
         pass
 
-    def get_converted(self):
-        # scrape the converted files from GRIN and update the DB
-        pass
+    def process_converted(self):
+        converted_barcodes = self.client.converted()
+
+        if converted_barcodes and len(converted_barcodes) > 0:
+            for barcode in converted_barcodes:
+                record = (
+                    self.db_manager.session.query(Record)
+                    .join(GRINStatus)
+                    .filter(
+                        Record.source_id == f"{barcode}|grin",
+                        GRINStatus.state != GRINState.DOWNLOADED.value,
+                    )
+                )
+
+                if record is not None:
+                    try:
+                        record.grin_status.state = GRINState.CONVERTED.value
+                        self.db_manager.commit_changes()
+                    except Exception:
+                        self.db_manager.session.rollback()
+                        raise
+
+if __name__ == "__main__":
+    # Run the process
+    grin_conversion = GRINConversion()
+    grin_conversion.process_converted()
