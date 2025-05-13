60510d0158775b14534e5c6450715e7030cd9f88
toxiapo <toxiapo@gmail.com>
2025-05-13 16:12:49 -0400

    batch process all books that are two days old

--- etl-pipeline/services/grin/download.py
+++ etl-pipeline/services/grin/download.py
@@ -1,10 +1,11 @@
-from datetime import datetime
-from typing import List
+from datetime import datetime, timedelta
+from typing import  List
 from model import GRINState, GRINStatus, Record
 from managers import DBManager, S3Manager
 from logger import create_log
 from scripts.grin_mirror import GRINClient
 from sqlalchemy import select, desc
+from util import chunk
 
 logger = create_log(__name__)
 
@@ -22,38 +23,39 @@ class GRINDownload:
 
     def run_process(self, batch_size=BATCH_SIZE_LIMIT, backfill=False):
         books: List[Record] = self._get_converted_books(batch_size, backfill)
-        books_done_processing: List[str] = []
 
-        for book in books:
-            barcode = book.source_id.split("|")[0]
-            file_name = f"{barcode}.tar.gz.gpg"
-            s3_key = f"grin/{file_name}"
+        for chunked_books in chunk(iter(books), batch_size):
+            books_done_processing: List[str] = []
+            for book in chunked_books:
+                barcode = book.source_id.split("|")[0]
+                file_name = f"{barcode}.tar.gz.gpg"
+                s3_key = f"grin/{file_name}"
 
-            try:
-                content = self._download(file_name)
-            except:
-                logger.exception(f"Error downloading content for {book}")
-                book.grin_status.failed_download += 1
-                self.db_manager.commit_changes()
-                continue
+                try:
+                    content = self._download(file_name)
+                except:
+                    logger.exception(f"Error downloading content for {book}")
+                    book.grin_status.failed_download += 1
+                    self.db_manager.commit_changes()
+                    continue
 
-            try:
-                self.s3_client.put_object(
-                    object=content,
-                    s3_key=s3_key,
-                    bucket=S3_BUCKET_NAME,
-                    storage_class="GLACIER_IR",
-                )
-            except Exception as e:
-                logger.exception(f"Error uploading to s3 for {book}")
-                continue
+                try:
+                    self.s3_client.put_object(
+                        object=content,
+                        s3_key=s3_key,
+                        bucket=S3_BUCKET_NAME,
+                        storage_class="GLACIER_IR",
+                    )
+                except Exception as e:
+                    logger.exception(f"Error uploading to s3 for {book}")
+                    continue
 
-            # Only update the `state` when download and upload_to_S3 operations succeeded
-            book.grin_status.state = GRINState.DOWNLOADED.value
-            books_done_processing.append(book)
+                # Only update the `state` when download and upload_to_S3 operations succeeded
+                book.grin_status.state = GRINState.DOWNLOADED.value
+                books_done_processing.append(book)
 
-        if len(books_done_processing) > 0:
-            self._update_states(books_done_processing)
+            if len(books_done_processing) > 0:
+                self._update_states(books_done_processing)
 
         self.db_manager.session.close()
 
@@ -70,16 +72,18 @@ class GRINDownload:
 
     def _get_converted_books(self, batch_size: int, backfill: bool):
         """Should return the DB object so that we can update the state directly"""
+        yesterday = datetime.now() - timedelta(days=1)
         query = (
             select(Record)
             .join(Record.grin_status)
             .where(GRINStatus.state == GRINState.CONVERTED.value)
+            .where(GRINStatus.date_modified >= yesterday)
             .order_by(desc(Record.date_modified))
-            .limit(batch_size)
         )
 
         if backfill:
             query = query.where(GRINStatus.date_created <= datetime(1991, 8, 25))
+            query = query.limit(batch_size)
 
         books = self.db_manager.session.execute(query).scalars().all()
         return books
