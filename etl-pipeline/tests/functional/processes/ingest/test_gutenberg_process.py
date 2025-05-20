9d78385b5f59ab84a9a0ecc6f18f880beee26324
kyle <kylejacksonvillegas@nypl.org>
2025-05-20 16:26:22 -0400

    NO-REF: Fix gutenberg test

--- etl-pipeline/tests/functional/processes/ingest/test_gutenberg_process.py
+++ etl-pipeline/tests/functional/processes/ingest/test_gutenberg_process.py
@@ -20,15 +20,18 @@ def test_gutenberg_process(
         db_manager=db_manager, storage_manager=s3_manager
     )
 
+    succeeded_records = []
+    failed_records = []
+
     for record in records:
-        succeeded_records = []
-        failed_records = []
         try:
-            record_file_saver.save_record_files(record)
+            record = record_file_saver.save_record_files(record)
         except Exception:
             failed_records.append(record)
         else:
             succeeded_records.append(record)
 
     assert_uploaded_manifests(succeeded_records)
-    assert all(record.state == RecordState.INGESTED for record in failed_records)
+
+    assert all(record.state == RecordState.INGESTED.value for record in failed_records)
+    assert all(record.state == RecordState.FILES_SAVED.value for record in succeeded_records)
