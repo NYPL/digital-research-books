88e3a7e696be6c515a0ea0c1d9c4b9f891f472a9
kyle <kylejacksonvillegas@nypl.org>
2025-06-02 15:38:20 -0400

    adding s3 logic

--- etl-pipeline/services/sources/grin_service.py
+++ etl-pipeline/services/sources/grin_service.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import os
-from typing import Optional
+from typing import Optional, Generator
 
 from managers import S3Manager
+from model import Record
 from .source_service import SourceService
 
 
@@ -11,7 +12,7 @@ class GRINService(SourceService):
         self.environment = os.environ["ENVIRONMENT"]
         self.file_locations = [
             f"drb-files-{self.environment}",
-            f"drb-files-{self.enviroment}-limited",
+            f"drb-files-{self.environment}-limited",
         ]
         self.storage_manager = S3Manager()
 
@@ -20,7 +21,20 @@ class GRINService(SourceService):
         start_timestamp: Optional[datetime] = None,
         offset: int = 0,
         limit: int = None,
-    ):
+    ) -> Generator[Record, None, None]:
+        for file_location in self.file_locations:
+            object_paginator = self.storage_manager.client.get_paginator('list_objects_v2')
+            object_iterator = object_paginator.paginate(Bucket=file_location, Prefix='/todo/add/prefix')
+            
+            if start_timestamp:
+                object_iterator = object_iterator.search(f"Contents[?to_string(LastModified) > '\"{start_timestamp}\"'].Key")
+            
+            for objects in object_iterator:
+                for content in objects['Contents']:
+                    key = content['Key']
+                    
+                    # TODO: get mets file, map to record, yield record
+
         return
 
     def get_record(self, record_id):
