d725381d958950438a190a6eb76d3a38223475a4
kyle <kylejacksonvillegas@nypl.org>
2025-06-04 11:05:23 -0400

    fix type hint

--- etl-pipeline/services/monitor.py
+++ etl-pipeline/services/monitor.py
@@ -79,7 +79,7 @@ def track_record_pipeline_message_failed(execution_time: float, message_body: st
     record_event(event_name, data=data)
 
 
-def track_records_ingested(number_of_records: str, source: str):
+def track_records_ingested(number_of_records: int, source: str):
     event_name = "RecordIngest:IngestCount"
     data = {"number_of_records": number_of_records, "source": source}
 
