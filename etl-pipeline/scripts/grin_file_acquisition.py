19504999a3c624de920a59c9406cde7be745052a
Priya Pai <priyapai@nypl.org>
2025-06-05 17:03:45 -0400

    Format lambda file

--- etl-pipeline/scripts/grin_file_acquisition.py
+++ etl-pipeline/scripts/grin_file_acquisition.py
@@ -3,12 +3,14 @@ import logging
 
 logger = logging.getLogger()
 
+
 def lambda_handler(event, context):
     # TODO: Add parsing of the actual StepFunction input to retrieve barcode correctly
     barcode = 33433115534525
 
     downloader = GRINDownload(barcode)
     downloader.run_process()
+
 
 if __name__ == "__main__":
     lambda_handler()
