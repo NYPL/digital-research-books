2f18dc5817569a3ccff624dfa815f96b14c0922f
kyle <kylejacksonvillegas@nypl.org>
2025-07-17 13:21:49 -0400

    formatting

--- etl-pipeline/utils/profiler.py
+++ etl-pipeline/utils/profiler.py
@@ -6,8 +6,8 @@ import psutil
 def get_process_memory():
     process = psutil.Process(os.getpid())
     mem_info = process.memory_info()
-    
-    return mem_info.rss / (1024 ** 2)
+
+    return mem_info.rss / (1024**2)
 
 
 def profile(logger):
@@ -18,10 +18,12 @@ def profile(logger):
             result = func(*args, **kwargs)
             mem_after = get_process_memory()
 
-            logger.info(f"{func.__name__} memory usage - before {mem_before:.2f}: MB, after: {mem_after:2f} MB")
+            logger.info(
+                f"{func.__name__} memory usage - before {mem_before:.2f}: MB, after: {mem_after:2f} MB"
+            )
 
             return result
-        
+
         return wrapper
-    
+
     return decorator
