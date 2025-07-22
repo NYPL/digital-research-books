2ac93af6953a5cbab37ede14fc5a49d3fa47180d
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-07-22 09:52:37 -0600

    Removed test class and comments

--- etl-pipeline/tests/functional/processes/test_grin_scrape.py
+++ etl-pipeline/tests/functional/processes/test_grin_scrape.py
@@ -1,16 +1,15 @@
 from processes.grin.grin_client import GRINClient
 
 
-class TestGRINScrape:
-    def test_url_and_request(self):
-        GRIN_SCRAPE_URL = "_all_books?book_state=NEW&book_state=PREVIOUSLY_DOWNLOADED&format=text"  # why previously downloaded?
+def test_url_and_request():
+    GRIN_SCRAPE_URL = (
+        "_all_books?book_state=NEW&book_state=PREVIOUSLY_DOWNLOADED&format=text"
+    )
 
-        grin_client = GRINClient()
-        expected_url = grin_client._url(GRIN_SCRAPE_URL)
-        response = grin_client.session.request("GET", expected_url, timeout=600)
-        response.raise_for_status()  # raises an error for bad responses, otherwise continues
+    grin_client = GRINClient()
+    expected_url = grin_client._url(GRIN_SCRAPE_URL)
+    response = grin_client.session.request("GET", expected_url, timeout=600)
+    response.raise_for_status()
 
-        assert GRIN_SCRAPE_URL in expected_url
-        assert response.content, (
-            "No content returned from GRIN endpoint"
-        )  # assert the response is not empty
+    assert GRIN_SCRAPE_URL in expected_url
+    assert response.content, "No content returned from GRIN endpoint"
