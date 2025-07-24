2343f833c515ffb6371325e9429616db80f3b63a
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-07-24 10:58:27 -0600

    Ruff format

--- etl-pipeline/tests/integration/test_grin_download.py
+++ etl-pipeline/tests/integration/test_grin_download.py
@@ -7,17 +7,22 @@ import pytest
 from managers import DBManager
 
 TEST_BUCKET = "drb-grin-files-test"
-TEST_BARCODE = "33433000009799" # barcode with converted status in local test db
+TEST_BARCODE = "33433000009799"  # barcode with converted status in local test db
+
 
 @pytest.fixture
 def db_manager():
     with DBManager() as manager:
         yield manager
 
+
 def test_s3_bucket():
     s3 = boto3.client("s3")
-    assert test_s3_bucket_read_access(s3, TEST_BUCKET), f"Bucket {TEST_BUCKET} is not readable"
+    assert test_s3_bucket_read_access(s3, TEST_BUCKET), (
+        f"Bucket {TEST_BUCKET} is not readable"
+    )
     # assert test_s3_bucket_write_access(s3, TEST_BUCKET), f"Bucket {TEST_BUCKET} is not writable" # uncomment to test write access
+
 
 def test_s3_bucket_read_access(s3, bucket_name):
     try:
@@ -27,6 +32,7 @@ def test_s3_bucket_read_access(s3, bucket_name):
     except ClientError as e:
         print(f"No read access to bucket '{bucket_name}': {e}")
         return False
+
 
 # uncomment to test write access
 # def test_s3_bucket_write_access(s3, bucket_name):
@@ -40,6 +46,7 @@ def test_s3_bucket_read_access(s3, bucket_name):
 #         print(f"No write access to bucket '{bucket_name}': {e}")
 #         return False
 
+
 def test_download_barcode(db_manager):
     # confirm barcode exists and has converted status in db
     grin_status = db_manager.session.get(GRINStatus, TEST_BARCODE)
@@ -50,7 +57,9 @@ def test_download_barcode(db_manager):
 
     # confirm bucket exists
     service = GRINDownloadService(TEST_BUCKET)
-    assert service.bucket == TEST_BUCKET, (f"Expected bucket name '{TEST_BUCKET}', but got {service.bucket}")
+    assert service.bucket == TEST_BUCKET, (
+        f"Expected bucket name '{TEST_BUCKET}', but got {service.bucket}"
+    )
 
     download_result = service.download_barcode(TEST_BARCODE)
     print(f"Download result: {download_result}")
