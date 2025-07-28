71d8c4a492b084b3be95dca585dc6595d5459a8a
roseb89 <dz4888@wayne.edu>
2025-07-28 16:00:08 -0400

    removed .all() and fixed some logic

--- etl-pipeline/tests/functional/processes/grin/test_conversion.py
+++ etl-pipeline/tests/functional/processes/grin/test_conversion.py
@@ -8,28 +8,24 @@ def test_convert_new_barcodes(barcodes, grin_client, db_manager):
     for barcode in barcodes:
         if not barcode:
             pytest.skip("No barcodes provided for testing.")
-        else:
-            conversion_process = GRINConversion(grin_client)    
-            conversion_process.runProcess()
 
-            
-            assert db_manager.session.query(
-                GRINStatus
+            conversion_process = GRINConversion(grin_client)
+            for barcode in barcodes:
+                conversion_process.runProcess()
+
+
+            assert db_manager.session.query(GRINStatus
             ).filter(GRINStatus.barcode.in_(barcodes)).count() > 0
-            assert all(
+            assert (
                 status.state == GRINState.CONVERTING.value
                 for status in db_manager.session.query(GRINStatus)
                 .filter(GRINStatus.barcode.in_(barcodes))
-                .all()
+                
             )
-            assert db_manager.session.query(
-                GRINStatus
+            assert db_manager.session.query(GRINStatus
             ).filter(GRINStatus.state == GRINState.PENDING_CONVERSION.value).count() > 0   
 
-            assert  db_manager.session.query(
-                GRINStatus
-            ).filter(GRINStatus.state == GRINState.CONVERTING.value).count() > 0 
-               
+          
         
 
 
