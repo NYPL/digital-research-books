685ed228db93f980f5b4b1ef51ea9d9c47f81d70
jacko villegas <86266231+kylevillegas93@users.noreply.github.com>
2025-07-28 14:14:57 -0400

    [SCHOL-81] Delete ES works that don't exist in DB (#830)

---
+++ etl-pipeline/scripts/sync_es_with_db.py
@@ -0,0 +1,45 @@
+import os
+
+from model import Work
+from managers import DBManager, ElasticsearchManager
+from elasticsearch_dsl import Search
+
+
+def main():
+    es_manager = ElasticsearchManager()
+    es_manager.create_elastic_connection()
+
+    index = os.environ["ELASTICSEARCH_INDEX"]
+    batch_size = 1000
+
+    with DBManager() as db_manager:
+        db_work_uuids = {
+            str(work[0])
+            for work in db_manager.session.query(Work.uuid).yield_per(10000)
+        }
+
+    to_delete = []
+    deleted_count = 0
+
+    for i, work in enumerate(
+        Search(index=index).query("match_all").params(size=10000).scan(), 1
+    ):
+        if work.uuid not in db_work_uuids:
+            to_delete.append(work.uuid)
+
+            if len(to_delete) >= batch_size:
+                deleted_count += len(to_delete)
+                es_manager.delete_work_records(to_delete)
+                to_delete = []
+
+        print(f"Synced {i:,} work documents, deleted {deleted_count}", end="\r")
+
+    if to_delete:
+        deleted_count += len(to_delete)
+        es_manager.delete_work_records(to_delete)
+
+    print(f"Synced {i:,} work documents, deleted {deleted_count}")
+
+
+if __name__ == "__main__":
+    main()
