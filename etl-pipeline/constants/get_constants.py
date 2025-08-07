42a273e20a6863cadde8afd581f47f017030080c
Clarissa Richard <107963214+clarissarichard@users.noreply.github.com>
2025-08-07 15:30:54 -0600

    SCHOL-77: Test GRIN download (#821)

--- etl-pipeline/constants/get_constants.py
+++ etl-pipeline/constants/get_constants.py
@@ -7,7 +7,7 @@ def get_constants():
     constants = {}
 
     for path in glob("./constants/*.json"):
-        file_name = re.search(r"\/([a-z0-9]+)\.json", path).group(1)
+        file_name = re.search(r"[\\/]+([a-z0-9]+)\.json", path).group(1)
 
         with open(path, "r") as constants_file:
             file_constants = json.load(constants_file)
