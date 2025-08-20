be0bac31f421a20e259f4306e716e334ac4561d7
jackiequach <jackiequach14@gmail.com>
2025-08-20 11:39:17 -0400

    update download tests to dynamically retrieve barcode

--- etl-pipeline/tests/functional/processes/grin/download_test.py
+++ etl-pipeline/tests/functional/processes/grin/download_test.py
@@ -10,8 +10,8 @@ from model import Record, FRBRStatus, RecordState, Source, GRINStatus, GRINState
 
 
 @pytest.fixture
-def grin_status(db_manager):
-    barcode = "33433011009234"
+def grin_status(db_manager, test_download_barcode):
+    barcode = test_download_barcode
 
     # Setup: Ensure a clean state before creating the new record
     db_manager.session.execute(delete(GRINStatus).where(GRINStatus.barcode == barcode))
