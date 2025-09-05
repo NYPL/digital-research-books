a4cbd2ac0316448f6c82b0163496a8ecbcedf62d
jackiequach <jackiequach14@gmail.com>
2025-09-05 12:28:31 -0400

    styling changes for ResultCard

--- web/src/components/ResearchAssistant/ResultCard.tsx
+++ web/src/components/ResearchAssistant/ResultCard.tsx
@@ -1,139 +1,134 @@
 import React from "react";
 import {
-  Card,
-  CardContent,
-  CardHeading,
-  Box,
-  CardActions,
-  Flex,
+    Box,
+    Flex,
+    Text,
+    Accordion,
+    Image,
+    Heading,
 } from "@nypl/design-system-react-components";
 import Link from "../Link/Link";
 import { Agent, WorkEdition } from "~/src/types/DataModel";
 import EditionCardUtils from "~/src/util/EditionCardUtils";
 import { PLACEHOLDER_COVER_LINK } from "~/src/constants/editioncard";
 import CardRequiredBadge from "../EditionCard/CardRequiredBadge";
-import CopyrightLink from "../EditionCard/CopyrightLink";
 import Ctas from "../EditionCard/Ctas";
 import FeaturedEditionBadge from "../EditionCard/FeaturedEditionBadge";
-import LanguageDisplayText from "../EditionCard/LanguageDisplayText";
-import PhysicalEditionBadge from "../EditionCard/PhysicalEditionBadge";
 import PublisherAndLocation from "../EditionCard/PublisherAndLocation";
 import ScanAndDeliverBlurb from "../EditionCard/ScanAndDeliverBlurb";
 import UpBlurb from "../EditionCard/UpBlurb";
+import PublicDomainBadge from "./PublicDomainBadge";
+import AuthorsList from "../AuthorsList/AuthorsList";
+import { ApiWork } from "~/src/types/WorkQuery";
 
 export const ResultCard: React.FC<{
-  authors: Agent[];
-  edition: WorkEdition;
-  title: string;
-  isFeaturedEdition?: boolean;
-}> = ({ authors, edition, title, isFeaturedEdition }) => {
-  const previewItem = EditionCardUtils.getPreviewItem(edition.items);
+    authors: Agent[];
+    edition: WorkEdition;
+    work: ApiWork;
+    isFeaturedEdition?: boolean;
+}> = ({ authors, edition, work, isFeaturedEdition }) => {
+    const previewItem = EditionCardUtils.getPreviewItem(edition.items);
 
-  const editionYearElem = (edition: WorkEdition) => {
-    const editionDisplay = EditionCardUtils.editionYearText(edition);
+    const editionYearElem = () => {
+        const editionDisplay = EditionCardUtils.editionYearText(edition);
+        const additionalEditions =
+            work.edition_count > 1 ? ` + ${work.edition_count - 1} more` : "";
 
-    const editionElem = edition ? (
-      <Link
-        to={{
-          pathname: `/edition/${edition.edition_id}`,
-          ...(previewItem
-            ? { query: { featured: previewItem.item_id } }
-            : null),
-        }}
-      >
-        {editionDisplay}
-      </Link>
-    ) : (
-      <>{editionDisplay}</>
+        return (
+            <>
+                {editionDisplay} <Link to={"/"}>{additionalEditions}</Link>
+            </>
+        );
+    };
+
+    const coverUrl = EditionCardUtils.getCover(edition.links);
+    const isPhysicalEdition = EditionCardUtils.isPhysicalEdition(previewItem);
+    const isUniversityPress = EditionCardUtils.isUniversityPress(previewItem);
+    const isLoginRequired = isPhysicalEdition || isUniversityPress;
+
+    return (
+        <Box
+            border="1px solid"
+            borderColor="ui.border.default"
+            padding="s"
+            backgroundColor="ui.white"
+            borderTop="2px solid"
+            borderTopColor="section.research.primary"
+        >
+            <Flex gap="s" flexDirection="column">
+                <Flex
+                    gap="xs"
+                    flexDirection="row"
+                    alignItems="center"
+                    marginBottom="xs"
+                >
+                    {isLoginRequired && <CardRequiredBadge />}
+                    {isFeaturedEdition && <FeaturedEditionBadge />}
+                    <PublicDomainBadge />
+                </Flex>
+                <Flex gap="s" flexDirection="row">
+                    <Image
+                        src={coverUrl}
+                        alt={
+                            coverUrl === PLACEHOLDER_COVER_LINK
+                                ? "Placeholder Cover"
+                                : `Cover for ${EditionCardUtils.editionYearText(edition)}`
+                        }
+                        size="xsmall"
+                        aspectRatio="original"
+                    />
+                    <Box>
+                        <Text size="caption" marginBottom="xxs">
+                            E-BOOK
+                        </Text>
+                        <Heading size="heading7">
+                            <Link
+                                to={{
+                                    pathname: `/work/${edition.edition_id}`,
+                                    ...(previewItem
+                                        ? { query: { featured: previewItem.item_id } }
+                                        : null),
+                                }}
+                                isUnderlined={false}
+                            >
+                                {work.title}
+                            </Link>
+                        </Heading>
+                        {authors.length > 0 && (
+                            <Box>
+                                By <AuthorsList authors={authors} />
+                            </Box>
+                        )}
+                        <Box>
+                            <PublisherAndLocation
+                                pubPlace={edition.publication_place}
+                                publishers={edition.publishers}
+                            />
+                        </Box>
+                        <Box>{editionYearElem()}</Box>
+                        {isPhysicalEdition && <ScanAndDeliverBlurb />}
+                        {isUniversityPress && <UpBlurb publishers={edition.publishers} />}
+                    </Box>
+                </Flex>
+                <Accordion
+                    width="100%"
+                    id={`accordion-summary-${edition.edition_id}`}
+                    accordionData={[
+                        {
+                            label: "Read summary",
+                            panel: <Text>{edition.summary || "No summary available."}</Text>,
+                        },
+                    ]}
+                />
+                <Flex
+                    flexDir="row"
+                    gap="xs"
+                >
+                    <Ctas authors={authors} item={previewItem} title={work.title} />
+                </Flex>
+            </Flex>
+        </Box>
     );
-    return editionElem;
-  };
-
-  const coverUrl = EditionCardUtils.getCover(edition.links);
-  const isPhysicalEdition = EditionCardUtils.isPhysicalEdition(previewItem);
-  const isUniversityPress = EditionCardUtils.isUniversityPress(previewItem);
-  const isLoginRequired = isPhysicalEdition || isUniversityPress;
-
-  return (
-    <Box
-      border="1px"
-      borderColor="ui.border.default"
-      padding="s"
-      paddingLeft={{ base: "l", md: null }}
-      paddingBottom="l"
-      paddingRight="l"
-    >
-      <Flex gap="xs" flexDirection={{ base: "column", lg: "row" }}>
-        {isLoginRequired && <CardRequiredBadge />}
-        {isFeaturedEdition && <FeaturedEditionBadge />}
-      </Flex>
-      <Card
-        id={`card-${edition.edition_id}`}
-        layout="column"
-        imageProps={{
-          src: coverUrl,
-          alt:
-            coverUrl === PLACEHOLDER_COVER_LINK
-              ? "Placeholder Cover"
-              : `Cover for ${EditionCardUtils.editionYearText(edition)}`,
-          size: "xsmall",
-          aspectRatio: "original",
-        }}
-        paddingTop="m"
-        flexFlow={{ md: "column nowrap", lg: "row" }}
-        justifyContent={{ md: "center", lg: "left" }}
-        sx={{
-          ".card-right": {
-            maxWidth: { base: "100%", lg: "200px" },
-            marginStart: { base: "0", lg: "m" },
-            marginTop: { base: "xs", lg: 0 },
-          },
-        }}
-      >
-        <CardHeading
-          level="h3"
-          size="heading6"
-          id="stack1-heading1"
-          sx={{
-            span: {
-              fontSize: "18px",
-              a: {
-                textDecoration: "none",
-              },
-            },
-          }}
-        >
-          <Flex alignItems="center" gap="xs">
-            <span>{editionYearElem(edition)}</span>
-            {isPhysicalEdition && <PhysicalEditionBadge />}
-          </Flex>
-        </CardHeading>
-        <CardContent>
-          <Box>
-            <PublisherAndLocation
-              pubPlace={edition.publication_place}
-              publishers={edition.publishers}
-            />
-            <LanguageDisplayText edition={edition} />
-            <CopyrightLink rights={previewItem?.rights} />
-          </Box>
-          {isPhysicalEdition && <ScanAndDeliverBlurb />}
-          {isUniversityPress && <UpBlurb publishers={edition.publishers} />}
-        </CardContent>
-        <CardActions
-          display="flex"
-          flexDir="column"
-          whiteSpace="nowrap"
-          sx={{
-            width: { base: "100%", lg: "200px" },
-          }}
-          gap="xs"
-        >
-          <Ctas authors={authors} item={previewItem} title={title} />
-        </CardActions>
-      </Card>
-    </Box>
-  );
 };
 
 export default ResultCard;
