1d541fbf285fea7b30625bd6cdc1b73b03206c1f
Jackie Quach <37321861+jackiequach@users.noreply.github.com>
2025-10-15 14:22:15 -0400

    SCHOL-140: Update GRINIngestProcess to have has_part (#868)
    * add has_part in GRIN ingest process
    
    * add has_part check
    
    * add page url
    
    * remove extra check
    
    * move code block out of try except
    
    * formatting

--- etl-pipeline/processes/grin/ingest.py
+++ etl-pipeline/processes/grin/ingest.py
@@ -1,19 +1,22 @@
+import io
+import json
+import os
+import re
+
 from datetime import datetime
 from logger import create_log
 from .download import GRINDownloadService
 from managers import SQSManager, S3Manager
-import os
-import json
-from model import Record, Source
+from model import Record, Source, FileFlags, Part
 from ..record_ingestor import RecordIngestor
 from services.rights_determiner import determine_rights
 from pymarc import parse_xml_to_array
-import io
-import re
 from mappings.marc_record import map_marc_record
 from time import sleep
 from .. import utils
 from utils.timer import timer
+from file_conversion.pdfs import mets_parser
+from digital_assets import get_stored_file_url
 
 SQS_VISIBILITY_TIMEOUT_SECS = 90 * 60
 logger = create_log(__name__)
@@ -75,10 +78,15 @@ class GRINIngestProcess:
             _, mets_file = self.grin_download_service.download_barcode(barcode)
 
             record = self._map_record(barcode, mets_file)
+            if self._is_in_public_domain(record):
+                skip_next = False
+            else:
+                skip_next = (
+                    self.params.options.get("skip_next", "false").lower() == "true"
+                )
             self.record_ingestor.ingest(
                 [record],
-                skip_next=self.params.options.get("skip_next", "false").lower()
-                == "true",
+                skip_next=skip_next,
             )
         except Exception:
             logger.exception(f"Failed to process barcode: {barcode}")
@@ -110,6 +118,11 @@ class GRINIngestProcess:
         except Exception:
             logger.exception(f"Failed to determine rights for barcode: {barcode}")
 
+        if self._is_in_public_domain(record) and record.has_part is None:
+            mets_file = mets_parser.METSFile.from_mets_str(xml_metadata)
+            first_page_part = self._create_first_page_part(barcode, mets_file)
+            record.has_part = [str(first_page_part)]
+
         return record
 
     def _is_in_public_domain(record) -> bool:
@@ -128,3 +141,18 @@ class GRINIngestProcess:
         public_domain_threshold_date = datetime.date(threshold_year, 1, 1)
 
         return publication_date < public_domain_threshold_date or is_public_domain
+
+    def _create_first_page_part(barcode: str, mets_file: mets_parser.METSFile) -> Part:
+        if not mets_file:
+            return None
+
+        return Part(
+            index=1,
+            url=get_stored_file_url(
+                storage_name=os.environ["PRIVATE_FILE_BUCKET"],
+                file_path=f"grin/{barcode}/{mets_file.first_page}",
+            ),
+            source=Source.GRIN.value,
+            file_type="application/ocr",
+            flags=str(FileFlags(reader=True)),
+        )
