f9be98d5157e2fca5abed4ccbaf0046a8d7db71c
Jackie Quach <37321861+jackiequach@users.noreply.github.com>
2025-10-15 13:22:47 -0400

    SCHOL-139: Add GRIN has_part script (#867)
    * [NO-REF] VRA e2e POC
    
    * clean
    
    * move embedder to top level
    
    * Updating VRA system prompt
    
    * simplify text pipeline
    
    * add hard-coded item view
    
    * wiring up api
    
    * fix linting and build
    
    * add preview button
    
    * fix typecheck error
    
    * update item search
    
    * add item search tool
    
    * fix preview link parsing
    
    * updating text pipeline
    
    * add itemid for item search tool
    
    * add item search ui and update api response
    
    * fix tool call and update system prompy
    
    * v0 prompt
    
    * fix tests
    
    * add viewer to item results
    
    * add itemId as dependency for sendMessage
    
    * uncomment tests and remove hardcoded values
    
    * remove unused hook
    
    * update dockerfile
    
    * revert dockerfile change
    
    * add setuptools to docker install
    
    * remove fasttest and revert dockerfile
    
    * skip download test
    
    * ruff
    
    * skip integration download test
    
    * ruff
    
    * add return to results button and cleanup
    
    * fix linting and more cleanup
    
    * fix test
    
    * update styling for back button
    
    * update download tests to dynamically retrieve barcode
    
    * Revert "update download tests to dynamically retrieve barcode"
    
    This reverts commit 0404663a1465d36bf76863373961e90cece6433d.
    
    * fix typos and cleanup
    
    * add not available text for default search results
    
    * SCHOL-120: Add unpacking service for web reader (#858)
    
    * add unpacking service to call from item_read
    
    * updates based on feedback
    
    * formatting
    
    * add todo comment
    
    * address some comments
    
    * add unit tests and fix error handling
    
    * cleanup tests
    
    * add memory profiler around unpack
    
    * fix test recursion error
    
    * formatting
    
    * add a tag for lifecycle rule to expire and delete files
    
    * update unit test
    
    * formatting
    
    * increase max_workers
    
    * resolve comments
    
    * address comments and remove console logs
    
    * add script to update has_part
    
    * filter only public domain records
    
    * address comment
    
    ---------
    
    Co-authored-by: kyle <kylejacksonvillegas@nypl.org>

---
+++ etl-pipeline/scripts/addHasPartToGRINRecords.py
@@ -0,0 +1,92 @@
+import os
+
+import boto3
+from sqlalchemy import func, or_
+import file_conversion.pdfs.mets_parser as mets_parser
+
+
+from logger import create_log
+from digital_assets.utils.get_stored_file_url import get_stored_file_url
+from model import Record, Part, Source, FileFlags, GRINStatus, GRINState
+from managers import DBManager, SQSManager
+
+
+logger = create_log(__name__)
+
+BATCH_SIZE = 1000
+
+
+def get_mets_file_from_s3(s3_client, barcode: str) -> mets_parser.METSFile:
+    key = f"grin/{barcode}/NYPL_{barcode}.xml"
+    try:
+        s3_object = s3_client.get_object(
+            Key=key,
+            Bucket=os.environ["PRIVATE_FILE_BUCKET"],
+        )
+        mets_str = s3_object["Body"].read()
+        return mets_parser.METSFile.from_mets_str(mets_str)
+    except Exception as e:
+        logger.error(f"Error fetching/parsing METS file for barcode {barcode}: {e}")
+        return None
+
+
+def create_first_page_part(barcode: str, mets_file: mets_parser.METSFile) -> Part:
+    if not mets_file:
+        return None
+
+    return Part(
+        index=1,
+        url=get_stored_file_url(
+            storage_name=os.environ["PRIVATE_FILE_BUCKET"],
+            file_path=f"grin/{barcode}/{mets_file.first_page}",
+        ),
+        source=Source.GRIN.value,
+        file_type="application/ocr",
+        flags=str(FileFlags(reader=True)),
+    )
+
+
+def main():
+    record_pipeline_queue = os.environ["RECORD_PIPELINE_SQS_QUEUE"]
+    sqs_manager = SQSManager(record_pipeline_queue)
+    s3_client = boto3.client("s3")
+
+    with DBManager() as db_manager:
+        query = (
+            db_manager.session.query(Record)
+            .join(GRINStatus, GRINStatus.record_id == Record.id)
+            .filter(
+                Record.source == "grin",
+                func.lower(Record.rights).contains("public domain"),
+                or_(Record.has_part == "{}", Record.has_part.is_(None)),
+                GRINStatus.state == GRINState.DOWNLOADED.value,
+            )
+        )
+
+        total_updated = 0
+
+        for record in query.yield_per(BATCH_SIZE):
+            try:
+                barcode = record.source_id.split("|")[0]
+                mets_file = get_mets_file_from_s3(s3_client, barcode)
+
+                if mets_file:
+                    first_page_part = create_first_page_part(barcode, mets_file)
+                    record.has_part = [str(first_page_part)]
+                    db_manager.session.add(record)
+                    total_updated += 1
+
+                    sqs_manager.send_message_to_queue(
+                        message={"source_id": record.source_id, "source": record.source}
+                    )
+            except Exception as e:
+                logger.error(
+                    f"Failed to process record with source_id {record.source_id}: {e}"
+                )
+
+        db_manager.session.commit()
+        logger.info(f"Updated {total_updated} GRIN records with has_part")
+
+
+if __name__ == "__main__":
+    main()
