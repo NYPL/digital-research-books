4d5fe84d533d69daa635bd56ba2e653137ec2b70
jackiequach <jackiequach14@gmail.com>
2025-12-02 14:57:51 -0500

    update svg props

--- web/src/components/ResearchAssistant/RewindIcon.tsx
+++ web/src/components/ResearchAssistant/RewindIcon.tsx
@@ -14,9 +14,9 @@ const RewindIcon: React.FC = () => {
                 <path
                     d="M1 6.25C1 6.25 2.50374 4.20116 3.72538 2.97868C4.94702 1.7562 6.6352 1 8.5 1C12.2279 1 15.25 4.02208 15.25 7.75C15.25 11.4779 12.2279 14.5 8.5 14.5C5.42268 14.5 2.82633 12.4407 2.01382 9.625M1 6.25V1.75M1 6.25H5.5"
                     stroke="white"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                 />
             </svg>
         </Icon>
