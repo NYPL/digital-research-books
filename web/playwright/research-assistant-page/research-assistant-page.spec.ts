4c8f3cd2612e522b8cd0943b425024e18e4925e8
Jeff Dohan <jdohan@outlook.com>
2026-01-13 17:10:51 -0500

    Add UI visibility checks

--- web/playwright/research-assistant-page/research-assistant-page.spec.ts
+++ web/playwright/research-assistant-page/research-assistant-page.spec.ts
@@ -1,7 +1,50 @@
-import { test } from "@playwright/test";
+import { Page, test } from "@playwright/test";
 import { ResearchAssistantPage } from "./research-assistant-page";
 
-test("Research Assistant page loads", async ({ page }) => {
+test("Research Assistant page opens and loads DOM", async ({ page }) => {
   const researchAssistantPage = new ResearchAssistantPage(page);
   await researchAssistantPage.navigateTo(), { waitUntil: 'domcontentloaded' };
+});
+
+test.describe("Research Assistant Page UI", () => {
+  let page: Page;
+  let researchAssistantPage: ResearchAssistantPage;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    researchAssistantPage = new ResearchAssistantPage(page);
+    await researchAssistantPage.navigateTo();
+  });
+
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  test("Research Assistant panel heading is visible", async () => {
+    await researchAssistantPage.researchAssistantPanelHeading.isVisible();
+  });
+
+  test("A message bubble from the agent is visible", async () => {
+    await researchAssistantPage.messageBubble.isVisible();
+  });
+
+  test("Chat input text box is visible", async () => {
+    await researchAssistantPage.chatInputTextBox.isVisible();
+  });
+
+  test("Results banner is visible", async () => {
+    await researchAssistantPage.resultsBanner.isVisible();
+  });
+
+  test("Empty search prompt is visible", async () => {
+    await researchAssistantPage.emptySearchPrompt.isVisible();
+  });
+
+  test("'Start over' button is visible", async () => {
+    await researchAssistantPage.startOverBtn.isVisible();
+  });
+
+  test("'Hide chat' button is visible", async () => {
+    await researchAssistantPage.hideChatBtn.isVisible();
+  });
 });
--- web/playwright/research-assistant-page/research-assistant-page.ts
+++ web/playwright/research-assistant-page/research-assistant-page.ts
@@ -1,10 +1,26 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 class ResearchAssistantPage {
   readonly page: Page;
+  readonly researchAssistantPanelHeading: Locator;
+  readonly messageBubble: Locator;
+  readonly chatInputTextBox: Locator;
+  readonly resultsBanner: Locator;
+  readonly emptySearchPrompt: Locator;
+  readonly startOverBtn: Locator;
+  readonly hideChatBtn: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.researchAssistantPanelHeading = page.getByRole("heading", {
+      name: "Virtual Research Assistant",
+    });
+    this.messageBubble = page.getByText("VRA:");
+    this.chatInputTextBox = page.getByRole("textbox", { name: "Type your response here" });
+    this.resultsBanner = page.getByText("public domain scholarly e-books from our collections");
+    this.emptySearchPrompt = page.getByRole("heading", { name: "Start searching to see results" });
+    this.startOverBtn = page.getByRole("button", { name: "Start over" });
+    this.hideChatBtn = page.getByRole('button', { name: 'Hide chat' });
   }
 
   async navigateTo() { await this.page.goto("/research-assistant"); }
