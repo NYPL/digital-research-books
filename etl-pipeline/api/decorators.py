c4f33439a872269a427c837ae341edd3431eec1c
Noah Chasek-Macfoy <bantucaravan@gmail.com>
2026-01-14 17:41:51 -0500

    NOREF/create-user-script (#914)
    script to create a basic auth user in DB via main.py --script protocol
    
    * add create user script
    
    * add inline comments
    
    * clarify basic auth decorator name

--- etl-pipeline/api/decorators.py
+++ etl-pipeline/api/decorators.py
@@ -1,8 +1,12 @@
 from base64 import b64decode
 from functools import wraps
+import os
+
 from flask import current_app, request
+
 from .db import DBClient
 from .utils import APIUtils
+
 from logger import create_log
 
 
@@ -28,17 +32,20 @@ def warn_deprecated(response, message):
     return response
 
 
-def require_token(func):
+def require_basic_authentication(func):
     @wraps(func)
     def decorator(*args, **kwargs):
-        logger.debug(request.headers)
+        # Only printing header keys to avoid writing passwords to logs
+        logger.debug(
+            f"Attempting basic authentication with request headers: {list(request.headers.keys())}"
+        )
 
         headers = {k.lower(): v for k, v in request.headers.items()}
 
         try:
             _, loginPair = headers["authorization"].split(" ")
             loginBytes = loginPair.encode("utf-8")
-            user, password = b64decode(loginBytes).decode("utf-8").split(":")
+            username, password = b64decode(loginBytes).decode("utf-8").split(":")
         except KeyError:
             return APIUtils.formatResponseObject(
                 403, "authResponse", {"message": "user/password not provided"}
@@ -47,7 +54,11 @@ def require_token(func):
         db_client = DBClient(current_app.config["DB_CLIENT"])
         db_client.createSession()
 
-        user = db_client.fetchUser(user)
+        user = db_client.fetchUser(username)
+        if user is None:
+            logger.debug(
+                f"User {username} not found in {os.environ['ENVIRONMENT']} database"
+            )
 
         if (
             not user
