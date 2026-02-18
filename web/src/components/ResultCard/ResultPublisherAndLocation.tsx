afd36f89525dcb968e0b6b83c3401bf078a5257f
jackiequach <jackiequach14@gmail.com>
2026-02-18 11:25:20 -0500

    update item card to match new designs

---
+++ web/src/components/ResultCard/ResultPublisherAndLocation.tsx
@@ -0,0 +1,47 @@
+import { Text } from "@nypl/design-system-react-components";
+import React from "react";
+import {
+    MAX_PLACE_LENGTH,
+    MAX_PUBLISHER_NAME_LENGTH,
+} from "~/src/constants/editioncard";
+import { Agent } from "~/src/types/DataModel";
+import { truncateStringOnWhitespace } from "~/src/util/Util";
+import EclipseSpacer from "./EclipseSpacer";
+
+const ResultPublisherAndLocation: React.FC<{
+  pubPlace: string;
+  publishers: Agent[];
+}> = ({ pubPlace, publishers }) => {
+  const displayLocation = pubPlace
+    ? truncateStringOnWhitespace(pubPlace, MAX_PLACE_LENGTH).trim()
+    : "";
+
+  const publisherNames: string[] = (publishers || [])
+    .map((p) => p && p.name)
+    .filter(Boolean) as string[];
+
+  let displayName = "";
+  if (publisherNames.length === 1) {
+    displayName = truncateStringOnWhitespace(
+      publisherNames[0],
+      MAX_PUBLISHER_NAME_LENGTH
+    );
+  } else if (publisherNames.length > 1) {
+    displayName = `${truncateStringOnWhitespace(
+      publisherNames[0],
+      MAX_PUBLISHER_NAME_LENGTH
+    )} + ${publisherNames.length - 1} more`;
+  }
+
+  if (!displayName && !displayLocation) return <></>;
+
+  return (
+    <>
+      {displayName && <Text as="span" whiteSpace="normal">{displayName}</Text>}
+      {displayName && displayLocation && <EclipseSpacer />}
+      {displayLocation && <Text as="span" whiteSpace="normal">{displayLocation}</Text>}
+    </>
+  );
+};
+
+export default ResultPublisherAndLocation;
