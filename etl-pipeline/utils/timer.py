b4873e4f5cc95e99edbdcb830d9af80012623058
Jeff Dohan <jdohan@outlook.com>
2026-03-04 03:54:17 -0500

    Use perf_counter for more accurate timing

--- etl-pipeline/utils/timer.py
+++ etl-pipeline/utils/timer.py
@@ -1,14 +1,14 @@
 import functools
-import time
+from time import perf_counter
 
 
 def timer(logger):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            start_time = time.time()
+            start_time = perf_counter()
             result = func(*args, **kwargs)
-            end_time = time.time()
+            end_time = perf_counter()
 
             logger.info(f"{func.__name__} took {end_time - start_time:.2f}s")
 
