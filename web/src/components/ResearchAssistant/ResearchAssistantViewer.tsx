4dec04100f7fd741ff6d7af3e99fb2aec2a8d51e
Jackie Quach <37321861+jackiequach@users.noreply.github.com>
2026-03-04 11:00:14 -0500

    SCHOL-305: Upgrade to web-reader to 5.0.1 (#969)
    * upgrade to latest web reader version
    
    * remove back button
    
    * wrap with DSProvider to provide styles
    
    * upgrade to 5.0.1
    
    * add mock for web-reader

--- web/src/components/ResearchAssistant/ResearchAssistantViewer.tsx
+++ web/src/components/ResearchAssistant/ResearchAssistantViewer.tsx
@@ -46,10 +46,10 @@ const ResearchAssistantViewer: React.FC<{
     <Box>
       {manifestApiUrl && (
         <WebReader
-          headerLeft={<></>}
           webpubManifestUrl={manifestApiUrl}
-          pdfWorkerSrc="/pdf-worker/pdf.worker.min.js"
+          pdfWorkerSrc="/pdf-worker/pdf.worker.min.mjs"
           injectablesFixed={injectables}
+          height="70vh"
         />
       )}
     </Box>
