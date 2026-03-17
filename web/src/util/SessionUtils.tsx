9526f67c6215ca1cfb198e3ee7931ca65e23b306
Jackie Quach <jackiequach14@gmail.com>
2026-03-17 15:24:50 -0400

    add fallback for generating uuid

--- web/src/util/SessionUtils.tsx
+++ web/src/util/SessionUtils.tsx
@@ -1,7 +1,18 @@
 export function getOrCreateSessionId(): string {
   let sessionId = sessionStorage.getItem("vraSessionId");
   if (!sessionId) {
-    sessionId = crypto.randomUUID();
+    if (typeof crypto.randomUUID === "function") {
+      sessionId = crypto.randomUUID();
+    } else {
+      sessionId = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        (c) => {
+          const r = (Math.random() * 16) | 0;
+          const v = c === "x" ? r : (r & 0x3) | 0x8;
+          return v.toString(16);
+        }
+      );
+    }
     sessionStorage.setItem("vraSessionId", sessionId);
   }
   return sessionId;
