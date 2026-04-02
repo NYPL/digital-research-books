49e36634c3a02c53cc893ecff817982efa69241a
Noah Chasek-Macfoy <bantucaravan@gmail.com>
2026-04-02 16:11:54 -0400

    replace compose setup with short script

---
+++ etl-pipeline/seed_local_db.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+./docker-compose-healthcheck.sh
+
+source .venv/bin/activate
+python main.py -e local -p SeedLocalDataProcess -i daily
+
+#  ALT: make SeedLocalDataProcess fully idempotent so it won't alter the DB state if it's already run
+# then add seeddb to main docker compose file with `depends_on: devsetup; condition: service_completed_successfully`
+# ideas to make SeedLocalDataProcess idempotent: just  check if there are the same number of records present \
+#  in the db as the seed process fetches (50 records)
