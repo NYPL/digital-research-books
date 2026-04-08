2877328d67d1f6bc35d1d43e3c8c0d0f9d15b7d2
Jeff Dohan <jdohan@outlook.com>
2026-04-08 14:59:51 -0400

    Skip upon external connection failure

--- etl-pipeline/tests/integration/services/sources/test_dspace_service.py
+++ etl-pipeline/tests/integration/services/sources/test_dspace_service.py
@@ -1,10 +1,18 @@
+import pytest
+import requests
+
 from model import Source
 from services import get_source_service
 
 
 def test_get_records():
     dspace_service = get_source_service(Source.CLACSO.value)
-    records = dspace_service.get_records(limit=5)
 
-    for record in records:
-        assert record is not None
+    try:
+        records = dspace_service.get_records(limit=5)
+
+        for record in records:
+            assert record is not None
+    except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
+        pytest.skip(f"Skipping due to external connection issue: {e}")
+
