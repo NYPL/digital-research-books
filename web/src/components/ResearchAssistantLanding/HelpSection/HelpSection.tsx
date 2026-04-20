143488fee54da3c0ad5dd509e73617836db9925e
Jackie Quach <37321861+jackiequach@users.noreply.github.com>
2026-04-20 09:30:56 -0400

    resolve vqa items 22-24 (#1061)

--- web/src/components/ResearchAssistantLanding/HelpSection/HelpSection.tsx
+++ web/src/components/ResearchAssistantLanding/HelpSection/HelpSection.tsx
@@ -69,8 +69,12 @@ const HelpSection: React.FC<HelpSectionProps> = ({
                 display="flex"
                 alignItems="center"
                 gap="xxs"
+                _hover={{
+                  color: "section.research.primary",
+                  path: { stroke: "#00838A" },
+                }}
               >
-                Contact us <ArrowIcon direction="right" color="#006166" />
+                Get help <ArrowIcon direction="right" color="#006166" />
               </Link>
             </Flex>
           }
@@ -100,6 +104,10 @@ const HelpSection: React.FC<HelpSectionProps> = ({
                 display="flex"
                 alignItems="center"
                 gap="xxs"
+                _hover={{
+                  color: "section.research.primary",
+                  path: { stroke: "#00838A" },
+                }}
               >
                 <Text>Learn more</Text>
                 <ArrowIcon direction="right" color="#006166" />
