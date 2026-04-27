df071deda519e0475e3755b4bb38326ab5016c2c
Noah Chasek-Macfoy <bantucaravan@gmail.com>
2026-04-27 16:00:21 -0400

    unskip nypl source tests
    CI is now run on self-hosted runner that has
    needed access

--- etl-pipeline/tests/integration/services/sources/test_nypl_bib_service.py
+++ etl-pipeline/tests/integration/services/sources/test_nypl_bib_service.py
@@ -9,11 +9,8 @@ class TestNYPLBibService:
     def test_instance(self):
         return NYPLBibService()
 
-    # This test depends on NYPL_BIB_HOST env var which is in the NYPL AWS \
+    # NOTE: This test depends on NYPL_BIB_HOST env var which is in the NYPL AWS \
     # private subnet (i.e. VPN required for at home execution).
-    @pytest.mark.skipif(
-        os.getenv("IS_CI") == "true", reason="Skipping in CI environment"
-    )
     def test_get_records(self, test_instance: NYPLBibService):
         records = test_instance.get_records(
             start_timestamp=datetime.now(timezone.utc).replace(tzinfo=None)
