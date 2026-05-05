5524a58df52e8819d5443f5819671c3f94b75b25
Noah Chasek-Macfoy <bantucaravan@gmail.com>
2026-05-05 18:24:14 -0400

    add embedder import

--- etl-pipeline/vector_indexing/components/embedders/__init__.py
+++ etl-pipeline/vector_indexing/components/embedders/__init__.py
@@ -4,5 +4,12 @@ from vector_indexing.components.embedders.base import Embedder
 from vector_indexing.components.embedders.bedrock import BedrockEmbedder
 from vector_indexing.components.embedders.google import GoogleEmbedder
 from vector_indexing.components.embedders.qwen import QwenEmbedder
+from vector_indexing.components.embedders.sagemaker import SageMakerEmbedder
 
-__all__ = ["Embedder", "BedrockEmbedder", "GoogleEmbedder", "QwenEmbedder"]
+__all__ = [
+    "Embedder",
+    "BedrockEmbedder",
+    "GoogleEmbedder",
+    "QwenEmbedder",
+    "SageMakerEmbedder",
+]
