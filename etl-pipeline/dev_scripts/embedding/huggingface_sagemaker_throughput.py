37845ef076f88a863bb35bb449515d1fdee1fe0b
Noah Chasek-Macfoy <bantucaravan@gmail.com>
2026-05-07 23:23:37 -0400

    update qwen3 8b endpoint

--- etl-pipeline/dev_scripts/embedding/huggingface_sagemaker_throughput.py
+++ etl-pipeline/dev_scripts/embedding/huggingface_sagemaker_throughput.py
@@ -28,13 +28,15 @@ from transformers import AutoTokenizer
 # ---------------------------------------------------------------------------
 
 ENDPOINT_NAME = (
-    "tei-pplx-embed-v1-4b-ml-g6e-xlarge-20260506233640"  # pragma: allowlist secret
+    # "tei-pplx-embed-v1-4b-ml-g6e-xlarge-20260506233640"  # pragma: allowlist secret
+    "hf-tei-qwen3-embedding-4b-ml-g5-2xlarge-20260507-181318"  # pragma: allowlist secret
 )
 AWS_PROFILE = "vra-sandbox"
 
 TOTAL_REQUESTS = 200  # requests at each concurrency level
 # CONCURRENCY_LEVELS = [1, 16, 32, 64]
-CONCURRENCY_LEVELS = [1, 4, 8, 10, 13, 16]
+# CONCURRENCY_LEVELS = [1, 4, 8, 10, 13, 16]
+CONCURRENCY_LEVELS = [15, 20, 25, 30, 35]
 
 # TODO: for higher thruput. try  {inputs: [<str>, <str>]} (this works!),  batch_transform, async inference endpoint
 
