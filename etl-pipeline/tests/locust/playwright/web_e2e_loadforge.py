8cde793c977b14e4cb7932a07ae79a4b32c0144d
Jeff Dohan <jdohan@outlook.com>
2026-05-12 14:40:33 -0400

    Set request header for LF virtual user access

--- etl-pipeline/tests/locust/playwright/web_e2e_loadforge.py
+++ etl-pipeline/tests/locust/playwright/web_e2e_loadforge.py
@@ -94,6 +94,14 @@ class VRAUser(PlaywrightUser):
         page.set_default_timeout(TIMEOUT_MS)
         page.set_default_navigation_timeout(TIMEOUT_MS)
 
+        # Set request header needed for LoadForge virtual user access to QA site
+        if "qa" in self.host:
+            with open("files/loadforge-vu-access-key.txt", "r") as f:
+                loadforge_access_key = f.readline().strip()
+            await page.context.set_extra_http_headers(
+                {"X-LoadForge": loadforge_access_key}
+            )
+
         # 0. Authorize by setting token in localStorage to bypass login
         with open("files/authToken-vratestuser.txt", "r") as f:
             user_auth_token = f.readline().strip()
