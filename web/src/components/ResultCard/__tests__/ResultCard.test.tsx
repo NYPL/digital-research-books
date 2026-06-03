5fb6965c38f72852c2147f03e46399bc942d3287
Jackie Quach <jackiequach14@gmail.com>
2026-06-03 14:33:43 -0400

    use factory functions

--- web/src/components/ResultCard/__tests__/ResultCard.test.tsx
+++ web/src/components/ResultCard/__tests__/ResultCard.test.tsx
@@ -1,88 +1,23 @@
 import { render, screen } from "@testing-library/react";
+import {
+  createEdition,
+  createItem,
+  createWork,
+} from "~/src/__tests__/fixtures/CatalogSearchFixture";
 import { ResultPageProvider } from "~/src/context/ResultPageContext";
-import { CatalogEdition, PageType } from "~/src/types/ResearchAssistant";
+import { PageType } from "~/src/types/ResearchAssistant";
 import { ResultCard } from "../ResultCard";
 
-const mockAuthors = [
-  {
-    lcnaf: "",
-    name: "Test Author",
-    primary: "true",
-    viaf: "",
-  },
-];
-const mockEdition = {
+const mockEdition = createEdition({
   id: 1,
   title: "Test Title",
-  items: [
-    {
-      content_type: "ebook",
-      contributors: [],
-      drm: null,
-      edition_id: 1,
-      id: 2001,
-      links: [
-        {
-          content: null,
-          flags: {
-            reader: true,
-            catalog: false,
-            download: false,
-          },
-          id: 3002,
-          media_type: "application/ocr",
-          url: "https://example.org/reader/1",
-        },
-        {
-          content: null,
-          flags: {
-            embed: true,
-            catalog: false,
-            download: false,
-            reader: false,
-          },
-          id: 3003,
-          media_type: "text/html",
-          url: "https://example.org/embed/2",
-        },
-      ],
-      measurements: null,
-      physical_location: null,
-      rights: [
-        {
-          license: "public_domain",
-          rights_statement: "Public Domain",
-          source: "test-source",
-        },
-      ],
-      source: "grin",
-    },
-  ],
-  languages: [{ language: "English", iso_2: "en", iso_3: "eng" }],
-  links: [],
-  measurements: [],
-  publication_date: "2024",
-  publishers: [],
-  snippets: [
-    {
-      chunk_score: 0.5,
-      end_page: 10,
-      item_id: 2001,
-      start_page: 1,
-      text: "Test snippet",
-    },
-  ],
-  work_authors: mockAuthors,
   work_title: "Test Title",
   work_uuid: "1234",
-} as CatalogEdition;
+  items: [createItem()],
+});
 
-const mockWork = {
-  uuid: "1234",
-  title: "Test Title",
-  editions: [mockEdition],
-  edition_count: 1,
-};
+const mockAuthors = mockEdition.work_authors;
+const mockWork = createWork(mockEdition);
 
 const renderWithPage = (page: PageType) =>
   render(
