eb31e8afba0fe049c2e58eb9f3e8c52d628f4dab
alea12 <mail@alea12.net>
2026-06-04 12:08:21 -0400

    Skip rights determiner test due to API inconsistency

--- etl-pipeline/tests/integration/rights_determiner_test.py
+++ etl-pipeline/tests/integration/rights_determiner_test.py
@@ -3,6 +3,15 @@ import requests
 from services.rights_determiner import determine_rights
 
 
+@pytest.mark.skip(
+    reason="""
+    HathiTrust's `volumes/brief` API endpoint started returning inconsistent results.
+    Until we can determine the cause and implement a fix, this test will be skipped.
+    For reference, the API endpoint being tested is the following URL:
+
+    https://catalog.hathitrust.org/api/volumes/brief/htid/33433116781380.json
+    """
+)
 def test_determine_rights():
     try:
         rights = determine_rights("33433116781380")
