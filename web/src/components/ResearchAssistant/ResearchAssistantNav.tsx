332088c1772296929dc9ce8d01c48a6f07a6b1e2
nprotheroNYPL <nicolasprothero@nypl.org>
2026-06-30 16:11:42 -0400

    Fix wordage on 'My Account' button

--- web/src/components/ResearchAssistant/ResearchAssistantNav.tsx
+++ web/src/components/ResearchAssistant/ResearchAssistantNav.tsx
@@ -71,7 +71,7 @@ const ResearchAssistantNav: React.FC<ResearchAssistantNavProps> = ({
           </SubNavButton>
           <SubNavLink isOutlined href="#" id="subnav-my-account">
             <Icon name="actionIdentityFilled" size="medium" />
-            <Box display={{ base: "none", md: "inline" }}>{"My account"}</Box>
+            <Box display={{ base: "none", md: "inline" }}>My account</Box>
           </SubNavLink>
         </>
       }
