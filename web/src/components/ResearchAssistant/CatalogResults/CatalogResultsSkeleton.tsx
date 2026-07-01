2c5e1e325c399d986351b47aa42e2fd871cdb71b
nprotheroNYPL <nicolasprothero@nypl.org>
2026-07-01 12:07:31 -0400

    fix skeleton header spacing

--- web/src/components/ResearchAssistant/CatalogResults/CatalogResultsSkeleton.tsx
+++ web/src/components/ResearchAssistant/CatalogResults/CatalogResultsSkeleton.tsx
@@ -16,17 +16,11 @@ export const CatalogResultsSkeleton: React.FC = () => {
   const accordionContentWidths = ["40%", "30%", "15%"];
 
   return (
-    <Flex
-      flexDir="column"
-      bgColor="ui.bg.default"
-      gap="s"
-      paddingX={{ base: "s", md: "l" }}
-    >
+    <Flex flexDir="column" bgColor="ui.bg.default" gap="s">
       <Box
         bgColor="ui.bg.default"
         borderBottom="1px solid"
         borderColor="ui.border.default"
-        marginX="-2rem"
         paddingX="l"
         paddingY="s"
         marginLeft={MARGIN_BLEED}
@@ -39,98 +33,109 @@ export const CatalogResultsSkeleton: React.FC = () => {
           sx={{ div: { margin: 0 } }}
         />
       </Box>
-      <ResultsBanner />
-      {[1, 2].map((index) => (
-        <Box
-          key={index}
-          border="1px solid"
-          borderColor="ui.border.default"
-          padding="s"
-          backgroundColor="ui.white"
-          borderTop="2px solid"
-          borderTopColor="ui.gray.medium"
-          fontSize="desktop.body.body2"
-        >
-          <VStack align="left" spacing="s">
-            <Box>
-              <SkeletonLoader
-                showButton
-                showContent={false}
-                showHeading={false}
-                showImage={false}
-                maxWidth="120px"
-                margin={0}
-                sx={{
-                  div: { borderRadius: "4px", height: "27px" },
-                }}
-              />
-              <SkeletonLoader
-                showImage={false}
-                headingSize={1}
-                contentSize={2}
-                sx={{
-                  div: { borderRadius: "4px" },
-                }}
-              />
-            </Box>
-            <Box border="1px solid #E5E5E5" borderRadius="4px">
-              {accordionContentWidths.map((accordionWidth, index) => (
-                <Flex
-                  key={index}
-                  height="40px"
-                  padding="s"
-                  alignItems="center"
-                  justifyContent="space-between"
-                  borderBottom={index < 2 ? "1px solid #E5E5E5" : "none"}
-                >
-                  <Box width="100%">
-                    <SkeletonLoader
-                      showImage={false}
-                      showHeading={false}
-                      contentSize={1}
-                      margin={0}
-                      width={accordionWidth}
-                      sx={{
-                        div: { borderRadius: "4px", margin: 0, width: "100%" },
-                      }}
-                    />
-                  </Box>
-                  <Box>
-                    <SkeletonLoader
-                      layout="row"
-                      showHeading={false}
-                      showImage={false}
-                      contentSize={1}
-                      width="24px"
-                      margin={0}
-                      sx={{
-                        div: { width: "100%", height: "24px" },
-                      }}
-                    />
-                  </Box>
-                </Flex>
-              ))}
-            </Box>
-            <Flex gap="xs" height="40px">
-              {[1, 2].map((index) => (
+      <Flex
+        paddingX={{ base: "s", md: "l" }}
+        flexDir="column"
+        bgColor="ui.bg.default"
+        gap="s"
+      >
+        <ResultsBanner />
+        {[1, 2].map((index) => (
+          <Box
+            key={index}
+            border="1px solid"
+            borderColor="ui.border.default"
+            padding="s"
+            backgroundColor="ui.white"
+            borderTop="2px solid"
+            borderTopColor="ui.gray.medium"
+            fontSize="desktop.body.body2"
+          >
+            <VStack align="left" spacing="s">
+              <Box>
                 <SkeletonLoader
-                  key={index}
-                  layout="row"
                   showButton
                   showContent={false}
                   showHeading={false}
                   showImage={false}
-                  maxWidth="160px"
+                  maxWidth="120px"
                   margin={0}
+                  sx={{
+                    div: { borderRadius: "4px", height: "27px" },
+                  }}
+                />
+                <SkeletonLoader
+                  showImage={false}
+                  headingSize={1}
+                  contentSize={2}
                   sx={{
                     div: { borderRadius: "4px" },
                   }}
                 />
-              ))}
-            </Flex>
-          </VStack>
-        </Box>
-      ))}
+              </Box>
+              <Box border="1px solid #E5E5E5" borderRadius="4px">
+                {accordionContentWidths.map((accordionWidth, index) => (
+                  <Flex
+                    key={index}
+                    height="40px"
+                    padding="s"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    borderBottom={index < 2 ? "1px solid #E5E5E5" : "none"}
+                  >
+                    <Box width="100%">
+                      <SkeletonLoader
+                        showImage={false}
+                        showHeading={false}
+                        contentSize={1}
+                        margin={0}
+                        width={accordionWidth}
+                        sx={{
+                          div: {
+                            borderRadius: "4px",
+                            margin: 0,
+                            width: "100%",
+                          },
+                        }}
+                      />
+                    </Box>
+                    <Box>
+                      <SkeletonLoader
+                        layout="row"
+                        showHeading={false}
+                        showImage={false}
+                        contentSize={1}
+                        width="24px"
+                        margin={0}
+                        sx={{
+                          div: { width: "100%", height: "24px" },
+                        }}
+                      />
+                    </Box>
+                  </Flex>
+                ))}
+              </Box>
+              <Flex gap="xs" height="40px">
+                {[1, 2].map((index) => (
+                  <SkeletonLoader
+                    key={index}
+                    layout="row"
+                    showButton
+                    showContent={false}
+                    showHeading={false}
+                    showImage={false}
+                    maxWidth="160px"
+                    margin={0}
+                    sx={{
+                      div: { borderRadius: "4px" },
+                    }}
+                  />
+                ))}
+              </Flex>
+            </VStack>
+          </Box>
+        ))}
+      </Flex>
     </Flex>
   );
 };
